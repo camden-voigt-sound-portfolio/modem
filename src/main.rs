77d31aa2d07ca44a24f1b33127a04420532473a0
Camden Voigt <camdenvoigt@gmail.com>
2026-04-27 15:45:12 -0700

    Some cleanup

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,10 @@ struct Args {
     /// Use test file 2
     #[arg(long)]
     test2: bool,
+
+    /// toggle debug mode ON
+    #[arg(short, long)]
+    debug: bool,
 }
 
 fn main() {
@@ -35,12 +39,15 @@ fn main() {
         input_file_name = "input/test2.wav";
     }
     let output_file_name = args.output.as_str();
+    let debug = args.debug;
 
     // Get blocks of samples
     let mut input = WavReader::open(input_file_name).unwrap();
 
     let num_blocks: usize = input.len() as usize / 160;
-    println!("{} - {}", input.len(), num_blocks);
+    if debug {
+        println!("num_samples: {}\n num_blocks: {}", input.len(), num_blocks);
+    }
     let mut blocks: Vec<Vec<i16>> = Vec::with_capacity(num_blocks);
     for _ in 0..num_blocks {
         blocks.push(Vec::new())
@@ -53,11 +60,17 @@ fn main() {
 
     // Get bits by doing power
     let mut bits: Vec<i16> = Vec::new();
+    if debug {
+        println!("Power Diffs");
+        println!("-------------");
+    }
     for (i, block) in blocks.iter().enumerate() {
         let mark_power = tone_power(block, 2225.0);
         let space_power = tone_power(block, 2025.0);
         let diff = mark_power - space_power;
-        println!("{}: {} - {} = {}", i, mark_power, space_power, diff);
+        if debug {
+            println!("{}: {} - {} = {}", i, mark_power, space_power, diff);
+        }
 
         if diff < 0.0 {
             bits.push(0);
@@ -69,29 +82,40 @@ fn main() {
     }
 
     // convert bits to characters
+    if debug {
+        println!("Bytes");
+        println!("----------");
+    }
     let mut message = String::from("");
     let mut bs = bits.as_slice();
     for _ in 0..=((num_blocks / 10) - 1) {
         let (byte, bits) = bs.split_at(10);
         bs = bits;
-        println!("{:?}", byte);
         assert!(byte[0] == 0);
         assert!(byte[9] == 1);
         let mut num: u8 = 0;
+        // Go through actual data bits in reverse order since they come in LSB first
         for bit in (1..=8).rev() {
             num <<= 1;
             num |= byte[bit] as u8;
         }
-        println!("num - {:?}", num);
+        if debug {
+            println!("{:?}", byte);
+            println!("num - {:?}", num);
+        }
         message.push(num as char);
     }
 
     // Write to file
-    println!("{}", message);
+    println!(
+        "Final Message (saved in {}) - {}",
+        output_file_name, message
+    );
     let mut file = File::create(output_file_name).unwrap();
     file.write_all(message.as_bytes()).unwrap();
 }
 
+// Calculate i/q power for a give set of samples and frequency
 fn tone_power(samples: &[i16], freq: f32) -> f32 {
     let mut i = 0.0;
     let mut q = 0.0;
